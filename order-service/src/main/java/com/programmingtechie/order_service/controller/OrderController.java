cfdd23f55add7e6df5380a882cf4808f82b2c792
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-06-20 17:08:17 +0200

    ft: add Resilence4j circuit breaker

--- order-service/src/main/java/com/programmingtechie/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/programmingtechie/order_service/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.programmingtechie.order_service.controller;
 import com.programmingtechie.order_service.DTO.OrderRequest;
 import com.programmingtechie.order_service.model.Order;
 import com.programmingtechie.order_service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +15,13 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "placeOrderFallback")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
          orderService.placeOrder(orderRequest);
          return "Order Placed Successfully";
+    }
+
+    public String placeOrderFallback(OrderRequest orderRequest, RuntimeException e) {
+        return "Order Service is down. Please try again later";
     }
 }
