a11934c866ae494a954940dc31e02b4586f7a60b
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-06-21 17:24:12 +0200

    ft: add centralised tracing using micrometer and zipkin

--- inventory-service/src/main/java/com/programmingtechie/inventory_service/service/InventoryService.java
+++ inventory-service/src/main/java/com/programmingtechie/inventory_service/service/InventoryService.java
@@ -19,9 +19,9 @@ public class InventoryService {
     @Transactional(readOnly = true)
     @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCodes) {
-        log.info("Wait started");
-        Thread.sleep(10000);
-        log.info("Wait ended");
+//        log.info("Wait started");
+//        Thread.sleep(10000);
+//        log.info("Wait ended");
         return inventoryRepository.findBySkuCodeIn(skuCodes).stream().map(inventory ->
             InventoryResponse.builder()
                     .skuCode(inventory.getSkuCode())
